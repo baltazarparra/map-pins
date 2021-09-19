ee7f690a378d735fa1607a3246ee4c83f052ae21
baltazarparra <baltazar.parra.ext@dasa.com.br>
2021-09-19 19:02:27 -0300

    pwa

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,6 +10,8 @@ function App({ Component, pageProps }: AppProps) {
         <title>Boilerplate</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#666" />
         <meta name="description" content="Boilerplate" />
       </Head>
       <GlobalStyles />
