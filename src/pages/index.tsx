e12f45a31c4ee3197d0616f9466db5bc748447a3
baltz <baltazarparra@outlook.com>
2021-10-04 07:08:48 -0300

    Add linkwrapper

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,19 +3,5 @@ import dynamic from 'next/dynamic'
 const Map = dynamic(() => import('components/Map'), { ssr: false })
 
 export default function Home() {
-  return (
-    <Map
-      places={[
-        {
-          id: '666',
-          name: 'Parque Campolim',
-          slug: 'campolim',
-          location: {
-            latitude: -23.5293,
-            longitude: -47.4711
-          }
-        }
-      ]}
-    />
-  )
+  return <Map />
 }
